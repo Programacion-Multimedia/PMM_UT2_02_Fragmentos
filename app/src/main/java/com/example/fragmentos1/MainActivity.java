59af6e3c94e6ec5ea258aa3eb1552b521d6650bd
bananaboy448 <zerobananaboy@gmail.com>
2025-12-14 18:33:14 +0100

    commit

--- app/src/main/java/com/example/fragmentos1/MainActivity.java
+++ app/src/main/java/com/example/fragmentos1/MainActivity.java
@@ -15,7 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
 
 import com.example.fragmentos1.placeholder.PlaceholderContent;
 
